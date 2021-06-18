833deb45b192885e608f5f9fc164479237240fff
rath3t <rathet@gmail.com>
2021-06-18 08:58:43 +0200

    Update FindFilesystem.cmake

--- FindFilesystem.cmake
+++ FindFilesystem.cmake
@@ -241,7 +241,7 @@ endif()
 cmake_pop_check_state()
 
 set(Filesystem_FOUND ${_found} CACHE BOOL "TRUE if we can run a program using std::filesystem" FORCE)
-
+set(Filesystem_FOUND TRUE)
 if(Filesystem_FIND_REQUIRED AND NOT Filesystem_FOUND)
     message(FATAL_ERROR "Cannot run simple program using std::filesystem")
 endif()
